02aaa7eb1ba6a655e5648b12c144909d1066f898
NguyenNguyen <nguyenbk92@gmail.com>
2020-10-23 03:20:52 +0000

    v1.0.4: get price viaExactToken

--- index.js
+++ index.js
@@ -111,6 +111,57 @@
 
  }
 
+ const getMidPriceViaExactToken = async (baseToken, baseDecimal, quoteToken, quoteDecimal, midToken, midDecimal, chainId, infuraKey) => {
+     if (chainId == undefined) {
+         chainId = ChainId.MAINNET
+     }
+     let network
+     if (infuraKey != undefined) {
+         network = new InfuraProvider(getNetwork(chainId), infuraKey)
+     } else {
+         network = getDefaultProvider(getNetwork(chainId))
+     }
+
+     let base = new Token(chainId, baseToken, baseDecimal),
+         quote = new Token(chainId, quoteToken, quoteDecimal),
+         mid = new Token(chainId, midToken, midDecimal)
+         quoteMid = await Fetcher.fetchPairData(quote, mid, network),
+         midBase = await Fetcher.fetchPairData(mid, base, network),
+         route = await new Route([midBase, quoteMid], base),
+         base2quote = await route.midPrice.toSignificant(6),
+         quote2base = await route.midPrice.invert().toSignificant(6)
+
+     return {
+         base2quote: base2quote,
+         quote2base: quote2base
+     }
+
+ }
+ const getExecutionPriceViaExactToken = async (baseToken, baseDecimal, quoteToken, quoteDecimal, midToken, midDecimal, tradeAmount, chainId, infuraKey) => {
+     if (chainId == undefined) {
+         chainId = ChainId.MAINNET
+     }
+     let network
+     if (infuraKey != undefined) {
+         network = new InfuraProvider(getNetwork(chainId), infuraKey)
+     } else {
+         network = getDefaultProvider(getNetwork(chainId))
+     }
+     let base = new Token(chainId, baseToken, baseDecimal),
+         quote = new Token(chainId, quoteToken, quoteDecimal),
+ 	 mid = new Token(chainId, midToken, midDecimal)
+         quoteMid = await Fetcher.fetchPairData(quote, mid, network),
+         midBase = await Fetcher.fetchPairData(mid, base, network),
+         route = await new Route([midBase, quoteMid], base),
+         base2quote = await route.midPrice.toSignificant(6),
+         quote2base = await route.midPrice.invert().toSignificant(6),
+         trade = new Trade(route, new TokenAmount(base, tradeAmount), TradeType.EXACT_INPUT)
+
+     return trade.executionPrice.toSignificant(6)
+
+ }
+
+
  const main = async () => {
      let data
      data = await getMidPrice("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", 18, "0x6B175474E89094C44Da98b954EedeAC495271d0F", 18)
@@ -129,5 +180,7 @@
      getMidPrice: getMidPrice,
      getExecutionPrice: getExecutionPrice,
      getMidPriceViaETH: getMidPriceViaETH,
-     getExecutionPriceViaETH: getExecutionPriceViaETH
+     getExecutionPriceViaETH: getExecutionPriceViaETH,
+     getMidPriceViaExactToken: getMidPriceViaExactToken,
+     getExecutionPriceViaExactToken: getExecutionPriceViaExactToken,
  }
